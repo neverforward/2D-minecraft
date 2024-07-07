3c9e7534d43e444c13684fc23a52802821c3f6de
扣税国王 <170108289+neverforward@users.noreply.github.com>
2024-07-07 22:29:57 +0800

    Add files via upload
    文件

---
+++ 2D minecraft.cpp
@@ -0,0 +1,311 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <conio.h>
+#include <chrono>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include "console-plus.h"
+
+// 清屏函数
+void ClearScreen() {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    DWORD dwConSize = csbi.dwSize.X * csbi.dwSize.Y;
+    COORD upperLeft = { 0, 0 };
+    DWORD dwCharsWritten;
+    FillConsoleOutputCharacter(hConsole, TCHAR(' '), dwConSize, upperLeft, &dwCharsWritten);
+    SetConsoleCursorPosition(hConsole, upperLeft);
+}
+char map[55][65]{
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','L','L','L','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','L','L','L','L','L','#','#','#','#','#','#','#','#','#','L','L','L','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','L','L','L','W','L','L','L','#','#','#','#','#','#','#','L','L','L','L','L','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','W','#','#','#','#','#','#','#','#','#','L','L','L','W','L','L','L','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','W','#','#','#','#','#','#','#','#','#','#','#','#','W','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+{'#','#','#','#','#','G','G','G','G','#','#','#','#','#','#','#','#','#','#','G','G','G','G','G','#','#','#','#','#','#','#','G','G','G','G','G','G','G','#','#','#','#','#','#','#','#','#','#','#','#','#','G','G','G','G','G','G','G','G','G'},
+{'G','G','G','G','G','M','M','M','M','G','G','G','G','G','G','G','G','G','G','M','M','M','M','M','G','G','G','G','G','G','G','M','M','M','M','M','M','M','G','G','G','G','G','G','G','G','G','G','G','G','G','M','M','M','M','M','M','M','M','M'},
+{'M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M'},
+{'M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','S','S','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M','M'},
+{'M','M','M','S','S','S','S','M','M','M','M','S','S','S','S','S','M','M','S','S','S','M','M','S','S','S','S','M','M','M','M','S','S','S','S','M','M','M','M','M','M','S','S','S','S','S','S','S','M','M','M','S','S','S','S','M','M','M','M','M'},
+{'S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S'},
+{'S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S'},
+{'S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S','S'}
+};
+int x=11, y=8;
+
+int block = 1; // 1 = STONE , 2 = GRASS
+
+void draw() {
+    for (int i = 0; i < 20; i++)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            if (i == x && j == y) {
+                greenbg();
+                white();
+                std::cout << "P";
+                reset();
+            }
+            else if (map[i][j]=='#') {
+                blue();
+                bluebg();
+                std::cout << "A";
+                reset();
+            }
+            else if (map[i][j] == 'S') {
+                rgb(128, 128, 128);
+                rgbbg(128, 128, 128);
+                std::cout << "S";
+                reset();
+            }
+            else if (map[i][j] == 'G') {
+                rgb(27, 115, 26);
+                rgbbg(27, 115, 26);
+                std::cout << "G";
+                reset();
+            }
+            else if (map[i][j] == 'M') {
+                rgb(120, 67, 21);
+                rgbbg(120, 67, 21);
+                std::cout << "G";
+                reset();
+            }
+            else if (map[i][j] == 'W') {
+                rgb(116, 90, 54);
+                rgbbg(116, 90, 54);
+                std::cout << "W";
+                reset();
+            }
+            else if (map[i][j] == 'L') {
+                rgb(117, 250, 141);
+                rgbbg(117, 250, 141);
+                std::cout << "L";
+                reset();
+            }
+            else if (map[i][j] == 'P') {
+                rgb(194, 157, 98);
+                rgbbg(194, 157, 98);
+                std::cout << "P";
+                reset();
+            }
+            else {
+                blue();
+                bluebg();
+                std::cout << "N";
+                reset();
+            }
+        }
+        std::cout << std::endl;
+    }
+    rgbbg(54, 126, 127);
+    std::cout << R"(
++-------+-------+-------+-------+-------+-------+
+|   1   |   2   |   3   |   4   |   5   |   6   |
+| STONE | GRASS |  MUD  | WOOD  | LEAF  | PLANK |
++-------+-------+-------+-------+-------+-------+
+                                                 
+w :   UP  d : RIGHT  j :  PLACE  q : exit        
+s : DOWN  a :  LEFT  k : REMOVE                  )" << std::endl << std::endl;
+    reset();
+    redbg();
+    std::cout << "x:" << x << "y:" << y;
+}
+auto now = std::chrono::system_clock::now();
+auto in_time_t = std::chrono::system_clock::to_time_t(now);
+std::stringstream ss;
+std::string formatted_time;
+bool logic(bool b,const char* fn) {
+    char c = _getch();
+    std::ofstream fout;
+    if (b) fout.open(fn, std::ios::app);
+    else fout.open("world/data-" + formatted_time + ".dat", std::ios::app);
+    if(c!='q') fout << c;
+    switch (c)
+    {
+    case 'a':
+    case 'A':
+        y--;
+        break;
+    case 'd':
+    case 'D':
+        y++;
+        break;
+    case 'w':
+    case 'W':
+        x--;
+        break;
+    case 's':
+    case 'S':
+        x++;
+        break;
+    case 'j':
+    case 'J':
+        if (block == 1) map[x][y + 1] = 'S';
+        else if (block == 2) map[x][y + 1] = 'G';
+        else if (block == 3) map[x][y + 1] = 'M';
+        else if (block == 4) map[x][y + 1] = 'W';
+        else if (block == 5) map[x][y + 1] = 'L';
+        else if (block == 6) map[x][y + 1] = 'P';
+        break;
+    case 'k':
+    case 'K':
+        map[x][y + 1] = '#';
+        break;
+    case 'q':
+    case 'Q':
+        fout.close();
+        return 1;
+    case '1':
+        block = 1;
+        break;
+    case '2':
+        block = 2;
+        break;
+    case '3':
+        block = 3;
+        break;
+    case '4':
+        block = 4;
+        break;
+    case '5':
+        block = 5;
+        break;
+    case '6':
+        block = 6;
+        break;
+    default:
+        std::cout << "\a";
+        break;
+    }
+    if (x >= 20 || x < 0 || y>=60 || y < 0) {
+        x = 11;y = 8;
+        std::cout << "\a";
+    }
+    fout.close();
+    ClearScreen();
+    return 0;
+}
+
+// 主程序
+int main() {
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%H-%M-%S");
+    formatted_time = ss.str();
+    hidecur();
+    italic();
+    std::cout << R"(2D minecraft
+by github@neverforward
+)" << std::endl;
+    reset();
+    redbg();
+    std::cout << R"(
+1. new world
+2. open file(world/data-year-month-day-hour-min-sec.dat)
+)";
+    if (_getch() == '1') {
+        ClearScreen();
+        while (1)
+        {
+            draw();
+            if (logic(false,"")) {
+                reset();
+                return 0;
+            }
+        }
+    }
+    else {
+        reset();
+        std::cout << "file name:";
+        char ch[10010];
+        std::cin >> ch;
+        std::ifstream fin(ch, std::ios::in);
+        if (!fin.is_open()) {
+            redbg();
+            std::cerr << "Error opening file." << std::endl;
+            reset();
+            _getch();
+            return -1;
+        }
+        char c;
+        while (fin.get(c)) {
+            switch (c)
+            {
+            case 'a':
+            case 'A':
+                y--;
+                break;
+            case 'd':
+            case 'D':
+                y++;
+                break;
+            case 'w':
+            case 'W':
+                x--;
+                break;
+            case 's':
+            case 'S':
+                x++;
+                break;
+            case 'j':
+            case 'J':
+                if (block == 1) map[x][y + 1] = 'S';
+                else if (block == 2) map[x][y + 1] = 'G';
+                else if (block == 3) map[x][y + 1] = 'M';
+                else if (block == 4) map[x][y + 1] = 'W';
+                else if (block == 5) map[x][y + 1] = 'L';
+                else if (block == 6) map[x][y + 1] = 'P';
+                break;
+            case 'k':
+            case 'K':
+                map[x][y + 1] = '#';
+                break;
+            case 'q':
+            case 'Q':
+                return 1;
+            case '1':
+                block = 1;
+                break;
+            case '2':
+                block = 2;
+                break;
+            case '3':
+                block = 3;
+                break;
+            case '4':
+                block = 4;
+                break;
+            case '5':
+                block = 5;
+                break;
+            case '6':
+                block = 6;
+                break;
+            default:
+                std::cout << "\a";
+                break;
+            }
+        }
+        fin.close(); 
+        ClearScreen();
+        while (1)
+        {
+            draw();
+            if (logic(true,ch)) {
+                reset();
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
---
+++ console-plus.h
@@ -0,0 +1,345 @@
+﻿// https://github.com/neverforward/console-plus
+
+#pragma once
+
+#include <stdio.h>
+
+// 光标移到x行y列
+void to(int x, int y) {
+	printf("\033[%d;%dH", x, y);
+}
+
+// 将光标移回(0,0)
+void toraw() {
+	printf("\033[H");
+}
+
+// 光标上移n行
+void up(int n) {
+	printf("\033[%dA", n);
+}
+
+// 光标下移n行
+void down(int n) {
+	printf("\033[%dB", n);
+}
+
+// 光标右移n列
+void right(int n) {
+	printf("\033[%dC", n);
+}
+
+// 光标左移n列
+void left(int n) {
+	printf("\033[%dD", n);
+}
+
+// 光标下移n行，移到行首
+void downbigin(int n) {
+	printf("\033[%dE", n);
+}
+
+// 光标上移n行，移到行首
+void upbigin(int n) {
+	printf("\033[%dF", n);
+}
+
+// 移动光标至列n
+void tocol(int n) {
+	printf("\033[%dG", n);
+}
+
+// 光标上移一行，按需可滚动
+void upline() {
+	printf("\033[M");
+}
+
+// 保存光标位置
+void savecur() {
+	printf("\033[7");
+}
+
+// 将光标恢复到上次保存的位置
+void restorecur() {
+	printf("\033[8");
+}
+
+// 清屏
+void cas() {
+	printf("\033[2J");
+}
+
+// 从光标擦到屏尾
+void ces() {
+	printf("\033[0J");
+}
+
+// 从光标擦到屏首
+void cbs() {
+	printf("\033[1J");
+}
+
+// 擦除保存的行
+void css() {
+	printf("\033[3J");
+}
+
+// 整行擦除
+void cal() {
+	printf("\033[2K");
+}
+
+// 从光标擦到行尾
+void cel() {
+	printf("\033[0k");
+}
+
+// 从光标擦到行首
+void cbl() {
+	printf("\033[1K");
+}
+
+// 粗体
+void bold() {
+	printf("\033[1m");
+}
+
+// 虚化
+void bokeh() {
+	printf("\033[2m");
+}
+
+// 斜体
+void italic() {
+	printf("\033[3m");
+}
+
+// 下划线
+void underline() {
+	printf("\033[4m");
+}
+
+// 闪烁
+void flashing() {
+	printf("\033[5m");
+}
+
+// 反色
+void inverted() {
+	printf("\033[7m");
+}
+
+// 隐形
+void invisible() {
+	printf("\033[8m");
+}
+
+// 删除线
+void strikethrough() {
+	printf("\033[9m");
+}
+
+// 重置
+void reset() {
+	printf("\033[0m");
+}
+
+// 黑
+void black() {
+	printf("\033[1;30m");
+}
+
+// 黑背景
+void blackbg() {
+	printf("\033[1;40m");
+}
+
+// 红
+void red() {
+	printf("\033[1;31m");
+}
+
+// 红背景
+void redbg() {
+	printf("\033[1;41m");
+}
+
+// 绿
+void green() {
+	printf("\033[1;32m");
+}
+
+// 绿背景
+void greenbg() {
+	printf("\033[1;42m");
+}
+
+// 黄
+void yellow() {
+	printf("\033[1;33m");
+}
+
+// 黄背景
+void yellowbg() {
+	printf("\033[1;43m");
+}
+
+// 蓝
+void blue() {
+	printf("\033[1;34m");
+}
+
+// 蓝背景
+void bluebg() {
+	printf("\033[1;44m");
+}
+
+// 品红
+void magenta() {
+	printf("\033[1;35m");
+}
+
+// 品红背景
+void magentabg() {
+	printf("\033[1;45m");
+}
+
+// 青
+void cyan() {
+	printf("\033[1;36m");
+}
+
+// 青背景
+void cyanbg() {
+	printf("\033[1;46m");
+}
+
+// 白
+void white() {
+	printf("\033[1;39m");
+}
+
+// 白背景
+void whitebg() {
+	printf("\033[1;49m");
+}
+
+// 亮黑
+void light_black() {
+	printf("\033[1;90m");
+}
+
+// 亮黑背景
+void light_blackbg() {
+	printf("\033[1;100m");
+}
+
+// 亮红
+void light_red() {
+	printf("\033[1;91m");
+}
+
+// 亮红背景
+void light_redbg() {
+	printf("\033[1;101m");
+}
+
+// 亮绿
+void light_green() {
+	printf("\033[1;92m");
+}
+
+// 亮绿背景
+void light_greenbg() {
+	printf("\033[1;102m");
+}
+
+// 亮黄
+void light_yellow() {
+	printf("\033[1;93m");
+}
+
+// 亮黄背景
+void light_yellowbg() {
+	printf("\033[1;103m");
+}
+
+// 亮蓝
+void light_blue() {
+	printf("\033[1;94m");
+}
+
+// 亮蓝背景
+void light_bluebg() {
+	printf("\033[1;104m");
+}
+
+// 亮品红
+void light_magenta() {
+	printf("\033[1;95m");
+}
+
+// 亮品红背景
+void light_magentabg() {
+	printf("\033[1;105m");
+}
+
+// 亮青
+void light_cyan() {
+	printf("\033[1;96m");
+}
+
+// 亮青背景
+void light_cyanbg() {
+	printf("\033[1;106m");
+}
+
+// 亮白
+void light_white() {
+	printf("\033[1;97m");
+}
+
+// 亮白背景
+void light_whitebg() {
+	printf("\033[1;109m");
+}
+
+/*
+256色(id >= 0 id <= 255)
+该表从最初的16种颜色(0∼15)开始。接下来的
+216 种颜色(16−231) 可能由偏移16 的 3bpc
+RGB 值组成，打包成一个值。
+*/
+void color256(short id) {
+	printf("\033[38;5;%dm", id);
+}
+
+/*
+256色背景(id >= 0 id <= 255)
+该表从最初的16种颜色(0∼15)开始。接下来的
+216 种颜色(16−231) 可能由偏移16 的 3bpc
+RGB 值组成，打包成一个值。
+*/
+void color256bg(short id) {
+	printf("\033[48;5;%dm", id);
+}
+
+// rgb色
+void rgb(int r, int g, int b) {
+	printf("\033[38;2;%d;%d;%dm", r, g, b);
+}
+
+// rgb背景
+void rgbbg(int r, int g, int b) {
+	printf("\033[48;2;%d;%d;%dm", r, g, b);
+}
+
+// 隐藏光标
+void hidecur() {
+	printf("\033[?25l");
+}
+
+// 显示光标
+void showcur() {
+	printf("\033[?25h");
+}
